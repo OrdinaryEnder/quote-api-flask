b2eb7328e9dbb40dd0a0f2f46bb173ccc6a33050
OrdinaryEnder <zairullahfreefire@outlook.com>
2022-09-01 08:27:38 +0700

    l9l

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ import os
 
 app = flask.Flask(__name__)
 
-quote = ["We just human, is a dirt whos given a life by god, Every Human Just Same and will die", "You can buy everything with money, but not happines", "Weeb anime has been broke your life, wheres your life path?", "Everyone has their own path, if that is a muslim, christian, or other we all just same", "If war never happened then the world is place with happiness", "I remember where my mom loves me, but she now gone."]
+quote = ["We just human, is a dirt whos given a life by god, Every Human Just Same and will die", "You can buy everything with money, but not happiness", "Weeb anime has been broke your life, wheres your life path?", "Everyone has their own path, if that is a muslim, christian, or other we all just same", "If war never happened then the world is place with happiness", "I remember where my mom loves me, but she now gone.", "Why your ruining ur life because of tiktok?", "World are just story that can be changed by ourself, Afterlife is the reality."]
 
 
 @app.route('/', methods=['GET'])
